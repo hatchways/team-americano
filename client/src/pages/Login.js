742a4bc838dc6ac598c3887166c1be5bc5999e01
Daniel Mau <daniel.b.mau@gmail.com>
2019-08-13 11:18:01 -0400

    Added login and signup pages

---
+++ client/src/pages/Login.js
@@ -0,0 +1,136 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/styles";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core";
+import withStyles from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Image from "../assets/bg-img.png";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100vh",
+    fontFamily: "Open Sans"
+  },
+  login: {
+    marginTop: "20px",
+    display: "flex",
+    direction: "row",
+    justify: "space-around",
+    alignItems: "center",
+    float: "right"
+  },
+  createButton: {
+    background: "white",
+    fontFamily: "Open Sans",
+    fontWeight: "600",
+    color: "#3A8DFF",
+    padding: "20px 70px 20px 70px",
+    marginLeft: "20px"
+  },
+  loginButton: {
+    backgroundColor: "#3A8DFF",
+    fontFamily: "Open Sans",
+    fontWeight: "600",
+    padding: "20px 70px 20px 70px",
+    marginTop: "50px"
+  },
+  speechIcon: {
+    color: "white",
+    textAlign: "center"
+  },
+  motto: {
+    display: "inline-block",
+    color: "white",
+    textAlign: "center",
+    marginTop: "40%",
+    fontSize: "200%"
+  },
+  image: {
+    backgroundImage: `linear-gradient(to bottom, rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0)), url(${Image})`,
+    backgroundRepeat: "no-repeat",
+    backgroundHeight: "100%",
+    backgroundPosition: "center",
+    backgroundSize: "100% 100%",
+    padding: "50px",
+    height: "100vh",
+  },
+  form: {
+    textAlign: "center",
+    display: "inline-block",
+    marginLeft: "50%",
+    marginTop: "20%"
+  }
+}));
+
+export default function Signup() {
+  const classes = useStyles();
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item container xs={false} sm={4} md={4}>
+        <div id="signUpContent" className={classes.image}>
+          <div className={classes.motto}>
+            <div className={classes.speechIcon}>
+              <i class="far fa-comment-dots fa-5x" />
+            </div>
+            <p>Converse with anyone with any language.</p>
+          </div>
+        </div>
+      </Grid>
+      <Grid item xs={6} sm={8} md={5}>
+        <div className={classes.login}>
+          <div>
+            <p>Don't have an account?</p>
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.createButton}
+          >
+            Create
+          </Button>
+        </div>
+        <Grid container>
+          <form className={classes.form}>
+            <h1>Welcome back!</h1>
+            <TextField
+              type="email"
+              variant="standard"
+              margin="normal"
+              required="true"
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              type="password"
+              variant="standard"
+              margin="normal"
+              required="true"
+              fullWidth
+              id="password"
+              label="Password"
+              name="password"
+              autoComplete="password"
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.loginButton}
+            >
+              Login
+            </Button>
+          </form>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,177 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/styles";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core";
+import withStyles from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Image from "../assets/bg-img.png";
+import Input from "@material-ui/core/Input";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import FilledInput from "@material-ui/core/FilledInput";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100vh",
+    fontFamily: "Open Sans"
+  },
+  login: {
+    marginTop: "20px",
+    display: "flex",
+    direction: "row",
+    justify: "space-around",
+    alignItems: "center",
+    float: "right"
+  },
+  loginButton: {
+    background: "white",
+    fontFamily: "Open Sans",
+    fontWeight: "600",
+    color: "#3A8DFF",
+    padding: "20px 70px 20px 70px",
+    marginLeft: "20px"
+  },
+  createButton: {
+    backgroundColor: "#3A8DFF",
+    fontFamily: "Open Sans",
+    fontWeight: "600",
+    padding: "20px 70px 20px 70px",
+    marginTop: "50px"
+  },
+  speechIcon: {
+    color: "white",
+    textAlign: "center",
+  },
+  motto: {
+    display: "inline-block",
+    color: "white",
+    textAlign: "center",
+    marginTop: "40%",
+    fontSize: "200%"
+  },
+  image: {
+    backgroundImage: `linear-gradient(to bottom, rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0)), url(${Image})`,
+    backgroundRepeat: "no-repeat",
+    backgroundHeight: "100%",
+    backgroundPosition: "center",
+    backgroundSize: "100% 100%",
+    padding: "50px",
+    height: "100vh",
+  },
+  form: {
+    textAlign: "center",
+    display: "inline-block",
+    marginLeft: "50%",
+    marginTop: "20%"
+  },
+  flag: {
+    width: "5%",
+    float: "left"
+  }
+}));
+
+export default function Signup() {
+  const classes = useStyles();
+
+  const [values, setValues] = React.useState({
+    language: "eng"
+  });
+
+  const handleChange = event => {
+    setValues(oldValues => ({
+      ...oldValues,
+      [event.target.name]: event.target.value
+    }));
+  };
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item container xs={false} sm={4} md={4}>
+        <div id="signUpContent" className={classes.image}>
+          <div className={classes.motto}>
+            <div className={classes.speechIcon}>
+              <i class="far fa-comment-dots fa-5x" />
+            </div>
+            <p>Converse with anyone with any language.</p>
+          </div>
+        </div>
+      </Grid>
+      <Grid item xs={6} sm={8} md={5}>
+        <div className={classes.login}>
+          <div>
+            <p>Already have an account?</p>
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.loginButton}
+          >
+            Login
+          </Button>
+        </div>
+        <Grid container>
+          <form className={classes.form}>
+            <h1>Create an account.</h1>
+            <TextField
+              type="email"
+              variant="standard"
+              margin="normal"
+              required="true"
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              type="password"
+              variant="standard"
+              margin="normal"
+              required="true"
+              fullWidth
+              id="password"
+              label="Password"
+              helperText="At least 6 characters."
+              name="password"
+              autoComplete="password"
+              inputProps={{ minLength: 6 }}
+            />
+            <FormControl fullWidth>
+              <InputLabel shrink htmlFor="primaryLanguage">
+                Select primary language.
+              </InputLabel>
+              <Select
+                value={values.language}
+                onChange={handleChange}
+                input={<Input name="language" id="primaryLanguage" />}
+                name="language"
+              >
+                <MenuItem value={"eng"}><img className={classes.flag} src={require("../assets/united-kingdom.svg")}/>English</MenuItem>
+                <MenuItem value={"fr"}><img className={classes.flag} src={require("../assets/france.svg")}/>Francais</MenuItem>
+                <MenuItem value={"spa"}><img className={classes.flag} src={require("../assets/spain.svg")}/>Espagnol</MenuItem>
+              </Select>
+            </FormControl>
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.createButton}
+            >
+              Create
+            </Button>
+          </form>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
