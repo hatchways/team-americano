ddca7ed91f11ba373cd1f2760a17637b484d22fe
Eric Hicks <eric.hicks.8989@gmail.com>
2019-08-15 19:27:45 -0400

    Fixed user tests

--- server/test/user.js
+++ server/test/user.js
@@ -20,40 +20,42 @@ describe("User", () => {
   });
 
   // Test Fetching Authenticated User:
-  it("It should return unauthorized since there is no logged in user", done => {
-    chai.request(app)
-      .get("/api/user")
-      .end((err, res) => {
-        res.should.have.status(401);
-      });
-      done();
-  });
-  it("It should fetch the currently authenticated user", async done => {
-    const user = {
-      name: "John Doe",
-      email: "email@gmail.com",
-      password: "password"
-    };
-
-    const token = new Promise((resolve, reject) => {
-      request(app)
-        .post("/api/auth/register")
-        .send(user)
+  describe("/GET user", () => {
+    it("It should return unauthorized since there is no logged in user", done => {
+      chai.request(app)
+        .get("/api/user")
         .end((err, res) => {
-          if (err) {
-            return reject(err);
-          }
-          return resolve(res.body.token);
+          res.should.have.status(401);
         });
+        done();
     });
+    it("It should fetch the currently authenticated user", async done => {
+      const user = {
+        name: "John Doe",
+        email: "email@gmail.com",
+        password: "password"
+      };
 
-    chai.request(app)
-      .get("/api/user")
-      .set("Authenticate", "Bearer " + token)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a("object");
+      const token = new Promise((resolve, reject) => {
+        request(app)
+          .post("/api/auth/register")
+          .send(user)
+          .end((err, res) => {
+            if (err) {
+              return reject(err);
+            }
+            return resolve(res.body.token);
+          });
       });
-      done();
+
+      chai.request(app)
+        .get("/api/user")
+        .set("Authenticate", "Bearer " + token)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+        });
+        done();
+    });
   });
 });
