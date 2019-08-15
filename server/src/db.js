c403d4dc5d55ea2564c572e488d753048ab7e333
Eric Hicks <eric.hicks.8989@gmail.com>
2019-08-15 19:11:01 -0400

    Update db.js

--- server/src/db.js
+++ server/src/db.js
@@ -5,13 +5,16 @@
 // Dependencies:
 const mongoose = require("mongoose");
 
+mongoose.Promise = global.Promise;
+
 // Connect to database:
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
-  useCreateIndex: true
-});
+  useCreateIndex: true,
+  useFindAndModify: false
+}).catch(e => console.log(e));
 
 // Log connection status to console:
 mongoose.connection.on("connected", () => console.log("Successfully connected to db."));
 mongoose.connection.on("disconnected", () => console.log("Successfully disconnected from db."));
-mongoose.connection.on("error", () => console.log("Error connecting to db."));
+mongoose.connection.on("error", (e) => console.log("Error connecting to db:", e));
