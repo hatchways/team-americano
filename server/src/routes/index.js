c1c4458bfb08580756f28e4d5721e22e90e88c65
Daniel Mau <daniel.b.mau@gmail.com>
2019-08-19 15:55:54 -0400

    Fixed socket implementation

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,29 +1,8 @@
 var express = require("express");
-var app = express();
 var router = express.Router();
 const passport = require("passport");
-var http = require("http").createServer(app); //require http
-var socket = require("socket.io"); //require socket.io
 
 require("../middleware/passport");
-
-app.get("/", function(req, res) {
-  res.sendFile("/home/daniel/team-americano/client/public/index.html");
-});
-
-var server = http.listen(3000, function() {
-  console.log("Listening on port 3000");
-});
-
-//Socket setup
-var io = socket(server);
-
-io.on("connection", function(socket) {
-  console.log("a user connected", socket.id);
-  socket.on("disconnect", function() {
-    console.log("user disconnected");
-  });
-}); //listen to the connection and disconnection events and console log a message
 
 router.get("/welcome", function(req, res, next) {
   res.status(200).send({ welcomeMessage: "Step 1 (completed)" });
