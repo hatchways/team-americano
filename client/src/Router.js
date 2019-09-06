afe7f74fd578442fa3014fb1c1c348741f086919
Daniel Mau <daniel.b.mau@gmail.com>
2019-09-05 21:26:21 -0400

    Redirect user to register page from referral link

--- client/src/Router.js
+++ client/src/Router.js
@@ -3,24 +3,24 @@
 // ==============================================
 
 // Dependencies:
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // Components:
-import { PrivateRoute } from './AuthRequired'
-import Register from './auth/Register';
-import Login from './auth/Login';
-import Home from './home/Home';
+import { PrivateRoute } from "./AuthRequired";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import Home from "./home/Home";
 
 // Router Component:
 export default class Router extends React.Component {
-
   render() {
     return (
       <BrowserRouter>
         <Switch>
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
+          <Route path="/join/:user_id" component={Register} />
           <PrivateRoute exact chat path="/chat" component={Home} />
           <PrivateRoute exact path="/" component={Home} />
         </Switch>
